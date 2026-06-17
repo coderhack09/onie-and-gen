424fe8c7a1c9142b1c8d12e95344fa7404456d5e
Rolando Valle <rolandovalle0428@gmail.com>
2026-06-17 20:56:40 +0800

    udpate

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next"
 
-const BASE_URL = "https://cristie-and-mark.weddinginvitationrsvp.com"
+const BASE_URL = "https://onie-and-gen.weddinginvitationrsvp.com"
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
